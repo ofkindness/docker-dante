ae406e62f3e7229a14df44750299eff7de58099d
Andrei Tretyakov <andrei.tretyakov@gmail.com>
2021-05-25 20:20:50 +0300

    Updarte

--- 1.4.2/sockd.conf.js
+++ 1.4.2/sockd.conf.js
@@ -1,7 +1,8 @@
-require('dotenv').load({ silent: true });
+const config = require('dotenv');
 
-console.log(
-`debug: ${process.env.debug || 0}
+config();
+
+console.log(`debug: ${process.env.debug || 0}
 
 internal: ${process.env.internal || '0.0.0.0'}  port = ${process.env.port || '1080'}
 external: ${process.env.external || 'eth0'}
@@ -22,5 +23,4 @@ socks pass {
     log: error
     protocol: tcp udp
     socksmethod: ${process.env.socksmethod || 'username'}
-}`
-);
+}`);
