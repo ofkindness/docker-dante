2cf2f887ae4db8598fcfae3556138e472c6e6daf
Andrei Tretyakov <andrei.tretyakov@gmail.com>
2021-05-26 12:06:24 +0300

    Update (#10)
    * Update Dockerfile
    
    * Add .gitignore
    
    * Create default login/password

--- 1.4.2/docker-entrypoint.sh
+++ 1.4.2/docker-entrypoint.sh
@@ -7,4 +7,9 @@ fi
 
 node /usr/src/dante/sockd.conf.js > /etc/sockd.conf
 
+export USERNAME=dante
+export PASSWORD=`pwgen -Bs1 12`
+
+adduser -D $USERNAME && echo $USERNAME:$PASSWORD | chpasswd && echo $PASSWORD
+
 exec "$@"
